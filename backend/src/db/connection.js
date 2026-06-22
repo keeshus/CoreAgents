694337f66e21ad90e04550436cf5292123e9f8f6
Kees Hus <keeshus@codeinfinity.nl>
2026-06-22 12:39:02 +0200

    cleanup: remove accidentally committed build artifacts from backend/src/db

--- backend/src/db/connection.js
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import pg from 'pg';
-import * as schema from './schema.js';
-const { Pool } = pg;
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-const db = drizzle(pool, { schema });
-export { db, pool };
