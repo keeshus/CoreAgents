bbad51c7acd66bcf2d200b60f7ddeafab69e722e
Kees Hus <keeshus@codeinfinity.nl>
2026-06-27 11:16:02 +0200

    fix: move Material Symbols stylesheet to _document.tsx, remove from _app.tsx Head

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,16 +8,12 @@ import { AssistantButton } from '@/components/assistant/AssistantButton';
 import { ThemeProvider, useTheme } from '@/hooks/useTheme';
 import { Icon } from '@/components/ui/Icon';
 import { Tooltip, TooltipProvider } from '@/components/ui/Tooltip';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <AuthProvider>
         <AssistantProvider>
-          <Head>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-          </Head>
           <TooltipProvider>
             <Component {...pageProps} />
             <AssistantGate />
